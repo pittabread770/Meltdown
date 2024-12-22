fa4d01f85b5ac40ba91103ad0ff0d9f6aec9823b
pittabread770 <pitt35@gmail.com>
2024-12-22 21:10:00 +0000

    Added irradiated debuff and dot
    also set up unity project for asset bundle creation

--- Meltdown/Meltdown.cs
+++ Meltdown/Meltdown.cs
@@ -2,12 +2,15 @@ using BepInEx;
 using R2API;
 using R2API.Utils;
 using RoR2;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace Meltdown
 {
     [BepInDependency(ItemAPI.PluginGUID)]
+    [BepInDependency(DotAPI.PluginGUID)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
 
     [BepInDependency(LanguageAPI.PluginGUID)]
@@ -19,6 +22,8 @@ namespace Meltdown
         public const string PluginName = "Meltdown";
         public const string PluginVersion = "0.1.0";
 
+        private static AssetBundle Assets;
+
         private static ItemDef reactorVents;
         private static ItemDef plutoniumArmaments;
         private static ItemDef damagedCoolingRod;
@@ -26,9 +31,15 @@ namespace Meltdown
         private static ItemDef volatileThoriumBattery;
         private static ItemDef uraniumFuelRod;
 
+        private static BuffDef irradiatedBuff;
+        private static DotController.DotDef irradiatedDot;
+        private static DotController.DotIndex irradiatedIndex;
+        private static Sprite irradiatedSprite;
+
         public void Awake()
         {
             Log.Init(Logger);
+            LoadAssets();
             SetupDebuffs();
             SetupItems();
         }
@@ -42,9 +53,50 @@ namespace Meltdown
             }
         }
 
+        private void LoadAssets()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Meltdown.meltdownassetbundle"))
+            {
+                Assets = AssetBundle.LoadFromStream(stream);
+            }
+
+            irradiatedSprite = Assets.LoadAsset<Sprite>("texBuffIrradiated.png");
+        }
+
         private void SetupDebuffs()
         {
-            // TODO: add irradiated debuff
+            irradiatedBuff = ScriptableObject.CreateInstance<BuffDef>();
+
+            irradiatedBuff.name = "MELTDOWN_IRRADIATED_NAME";
+            irradiatedBuff.canStack = true;
+            irradiatedBuff.isDebuff = true;
+            irradiatedBuff.eliteDef = null;
+            irradiatedBuff.isCooldown = false;
+            irradiatedBuff.isHidden = false; 
+            irradiatedBuff.buffColor = new Color32(255, 255, 0, 255);
+            irradiatedBuff.iconSprite = irradiatedSprite;
+
+            ContentAddition.AddBuffDef(irradiatedBuff);
+
+            irradiatedDot = new()
+            {
+                associatedBuff = irradiatedBuff,
+                resetTimerOnAdd = false,
+                interval = 1.0f,
+                damageColorIndex = DamageColorIndex.Poison, // TODO: create custom damage colour maybe?
+                damageCoefficient = 1.0f
+            };
+
+            DotAPI.CustomDotBehaviour irradiatedBehaviour = delegate (DotController controller, DotController.DotStack stack)
+            {
+                var attacker = stack.attackerObject?.GetComponent<CharacterBody>();
+                if (controller.victimBody != null && attacker != null)
+                {
+                    stack.damage = (attacker.damage * 1.5f) / 3.0f;
+                }
+            };
+
+            irradiatedIndex = DotAPI.RegisterDotDef(irradiatedDot, irradiatedBehaviour);
         }
 
         private void SetupItems()
@@ -60,26 +112,9 @@ namespace Meltdown
         private void SetupReactorVents()
         {
             reactorVents = ScriptableObject.CreateInstance<ItemDef>();
-
-            reactorVents.name = "MELTDOWN_REACTORVENTS_NAME";
-            reactorVents.nameToken = "MELTDOWN_REACTORVENTS_NAME";
-            reactorVents.pickupToken = "MELTDOWN_REACTORVENTS_PICKUP";
-            reactorVents.descriptionToken = "MELTDOWN_REACTORVENTS_DESC";
-            reactorVents.loreToken = "MELTDOWN_REACTORVENTS_LORE";
-
-#pragma warning disable Publicizer001
-            reactorVents._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier1Def.asset").WaitForCompletion();
-#pragma warning restore Publicizer001
-
-            reactorVents.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
-            reactorVents.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
-
-            reactorVents.canRemove = true;
-            reactorVents.hidden = false;
-
-            var displayRules = new ItemDisplayRuleDict(null); // TODO: reactor vents display
-
-            ItemAPI.Add(new CustomItem(reactorVents, displayRules));
+            setItemDef(reactorVents, "REACTORVENTS", ItemTier.Tier1, "RoR2/Base/Common/MiscIcons/texMysteryIcon.png", "RoR2/Base/Mystery/PickupMystery.prefab");
+            var reactorVentsDisplayRules = new ItemDisplayRuleDict(null); // TODO: reactor vents display
+            ItemAPI.Add(new CustomItem(reactorVents, reactorVentsDisplayRules));
             On.RoR2.CharacterBody.OnSkillActivated += CharacterBody_OnSkillActivated;
         }
 
@@ -106,6 +141,23 @@ namespace Meltdown
         private void SetupUraniumFuelRod()
         {
             // TODO
+        }
+
+        private void setItemDef(ItemDef itemDef, string langKey, ItemTier tier, string iconUrl, string modelUrl)
+        {
+            itemDef.name = $"MELTDOWN_{langKey}_NAME";
+            itemDef.nameToken = $"MELTDOWN_{langKey}_NAME";
+            itemDef.pickupToken = $"MELTDOWN_{langKey}_PICKUP";
+            itemDef.descriptionToken = $"MELTDOWN_{langKey}_DESC";
+            itemDef.loreToken = $"MELTDOWN_{langKey}_LORE";
+
+            itemDef.deprecatedTier = tier;
+
+            itemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>(iconUrl).WaitForCompletion();
+            itemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>(modelUrl).WaitForCompletion();
+
+            itemDef.canRemove = true;
+            itemDef.hidden = false;
         }
 
         private void CharacterBody_OnSkillActivated(On.RoR2.CharacterBody.orig_OnSkillActivated orig, CharacterBody self, GenericSkill skill)
@@ -139,7 +191,7 @@ namespace Meltdown
                     HurtBox hurtBox = GlobalEventManager.igniteOnKillHurtBoxBuffer[i];
                     if (hurtBox.healthComponent)
                     {
-                        DotController.InflictDot(hurtBox.healthComponent.gameObject, self.gameObject, DotController.DotIndex.Burn, 1.5f + 1.5f * (float)stack, 1f);
+                        DotController.InflictDot(hurtBox.healthComponent.gameObject, self.gameObject, irradiatedIndex, 3.0f + 3.0f * (float)stack, 1.0f);
                     }
                 }
                 GlobalEventManager.igniteOnKillHurtBoxBuffer.Clear();
