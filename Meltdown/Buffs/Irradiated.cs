387bec124e147f6fa02f816afc2aa4b9126fb275
pittabread770 <pitt35@gmail.com>
2025-01-02 23:23:46 +0000

    Added DoT effect for irradiated

--- Meltdown/Buffs/Irradiated.cs
+++ Meltdown/Buffs/Irradiated.cs
@@ -1,6 +1,8 @@
 ﻿using R2API;
 using RoR2;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Meltdown.Buffs
 {
@@ -10,13 +12,33 @@ namespace Meltdown.Buffs
         public DotController.DotDef dot;
         public DotController.DotIndex index;
         public Sprite sprite;
+        public BurnEffectController.EffectParams effectParams;
 
         public Irradiated()
         {
             sprite = Meltdown.Assets.LoadAsset<Sprite>("texBuffIrradiated.png");
 
+            SetupMaterial();
             SetupDebuff();
             SetupDot();
+
+            On.RoR2.DotController.UpdateDotVisuals += DotController_UpdateDotVisuals;
+        }
+
+        private void SetupMaterial()
+        {
+            var burnMaterial = Addressables.LoadAssetAsync<Material>("RoR2/Base/Common/matOnFire.mat").WaitForCompletion();
+            var irradiatedMaterial = new Material(burnMaterial);
+            var irradiatedColorRamp = Meltdown.Assets.LoadAsset<Texture2D>("texRampIrradiated.png");
+            irradiatedMaterial.SetTexture("_RemapTex", irradiatedColorRamp);
+
+            var burnEffect = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/FireEffect.prefab").WaitForCompletion();
+
+            effectParams = new BurnEffectController.EffectParams()
+            {
+                overlayMaterial = irradiatedMaterial,
+                fireEffectPrefab = burnEffect,
+            };
         }
 
         private void SetupDebuff()
@@ -48,6 +70,31 @@ namespace Meltdown.Buffs
             };
 
             index = DotAPI.RegisterDotDef(dot);
+        }
+
+        private void DotController_UpdateDotVisuals(On.RoR2.DotController.orig_UpdateDotVisuals orig, DotController self)
+        {
+            orig(self);
+
+            if (self.victimBody != null)
+            {
+                var modelLocator = self.victimBody.GetComponent<ModelLocator>();
+                var irradiatedController = self.victimBody.GetComponents<BurnEffectController>().FirstOrDefault(x => x.effectType == effectParams);
+
+                if (self.victimBody.HasBuff(buff))
+                {
+                    if (irradiatedController == default)
+                    {
+                        var irradiatedEffectController = self.victimBody.gameObject.AddComponent<BurnEffectController>();
+                        irradiatedEffectController.effectType = effectParams;
+                        irradiatedEffectController.target = modelLocator.modelTransform.gameObject;
+                    }
+                }
+                else if (irradiatedController != default)
+                {
+                    Object.Destroy(irradiatedController);
+                }
+            }
         }
     }
 }
